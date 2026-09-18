ac5f9065ced23a261fd131bc01c8d539c2d52816
Mariam <mariamomri.dev@gmail.com>
2026-09-18 09:14:43 +0200

    created pages

---
+++ src/app/pages/dashboard-medecin/dashboard-medecin.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-medecin',
+  imports: [],
+  templateUrl: './dashboard-medecin.html',
+  styleUrl: './dashboard-medecin.css',
+})
+export class DashboardMedecin {}
---
+++ src/app/pages/dashboard-patient/dashboard-patient.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-patient',
+  imports: [],
+  templateUrl: './dashboard-patient.html',
+  styleUrl: './dashboard-patient.css',
+})
+export class DashboardPatient {}
