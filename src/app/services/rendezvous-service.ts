1a70286a2f362a57a4a9c750f8776714914bf2ab
Mariam <mariamomri.dev@gmail.com>
2026-09-18 13:01:59 +0200

    improve style css

--- src/app/services/rendezvous-service.ts
+++ src/app/services/rendezvous-service.ts
@@ -21,4 +21,16 @@ export class RendezvousService {
   getByMedecinId(medecinId: string): Observable<RendezVous[]> {
     return this.http.get<RendezVous[]>(`${this.apiUrl}?medecinId=${medecinId}`);
   }
+
+  create(rendezvous: Omit<RendezVous, 'id'>): Observable<RendezVous> {
+    return this.http.post<RendezVous>(this.apiUrl, rendezvous);
+  }
+
+  update(id: string, rendezvous: Omit<RendezVous, 'id'>): Observable<RendezVous> {
+    return this.http.put<RendezVous>(`${this.apiUrl}/${id}`, rendezvous);
+  }
+
+  delete(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
