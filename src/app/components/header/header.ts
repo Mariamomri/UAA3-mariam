6c10c1068ba757bc5efa28f024d2c390d933b12c
Mariam <mariamomri.dev@gmail.com>
2026-09-18 10:10:20 +0200

    create code's header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterLink, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header implements OnInit {
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  estConnecte = false;
+
+  ngOnInit() {
+    this.verifierConnexion();
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      this.verifierConnexion();
+    });
+  }
+
+  verifierConnexion() {
+    this.estConnecte = localStorage.getItem('role') !== null;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.estConnecte = false;
+    this.router.navigate(['/login']);
+  }
+}
--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,6 +1,36 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Admin } from '../models/admin';
+import { Medecin } from '../models/medecin';
+import { Patient } from '../models/patient';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {}
+export class AuthService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:3000';
+
+  loginAdmin(email: string, password: string) {
+    return this.http.get<Admin[]>(
+      `${this.apiUrl}/admins?email=${email}&password=${password}`
+    );
+  }
+
+  loginMedecin(email: string, password: string) {
+    return this.http.get<Medecin[]>(
+      `${this.apiUrl}/medecins?email=${email}&password=${password}`
+    );
+  }
+
+  loginPatient(email: string, password: string) {
+    return this.http.get<Patient[]>(
+      `${this.apiUrl}/patients?email=${email}&password=${password}`
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('id');
+    localStorage.removeItem('role');
+  }
+}
